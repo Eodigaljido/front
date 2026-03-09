33372d672b8aae5f04e5e8725816e1e8fa6ab3a2
rjsgud49 <qkrrjsgud49@gmail.com>
2026-03-09 20:19:03 +0900

    refactor: tailwindcss적용

---
+++ Eodigaljido/types/nativewind.d.ts
@@ -0,0 +1,21 @@
+declare module 'react-native' {
+  interface ViewProps {
+    className?: string;
+  }
+  interface TextProps {
+    className?: string;
+  }
+  interface ScrollViewProps {
+    className?: string;
+  }
+  interface ImageProps {
+    className?: string;
+  }
+  interface TouchableOpacityProps {
+    className?: string;
+  }
+  interface SafeAreaViewProps {
+    className?: string;
+  }
+}
+
