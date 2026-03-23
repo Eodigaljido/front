4feedfc1b6993ff9cccf703a74cc96400c25ae91
rlaxogh76 <btm.email2769@gmail.com>
2026-03-23 15:09:24 +0900

    fix: nativewind.d.ts 주석 처리로 코드 정리

--- types/nativewind.d.ts
+++ types/nativewind.d.ts
@@ -1,31 +1,30 @@
-declare module 'react-native' {
-  interface ViewProps {
-    className?: string;
-  }
-  interface TextProps {
-    className?: string;
-  }
-  interface ScrollViewProps {
-    className?: string;
-  }
-  interface ImageProps {
-    className?: string;
-  }
-  interface TextInputProps {
-    className?: string;
-  }
-  interface PressableProps {
-    className?: string;
-  }
-  interface TouchableOpacityProps {
-    className?: string;
-  }
-  interface SafeAreaViewProps {
-    className?: string;
-  }
-  interface FlatListProps<ItemT> {
-    className?: string;
-    contentContainerStyle?: object;
-  }
-}
-
+// declare module 'react-native' {
+//   interface ViewProps {
+//     className?: string;
+//   }
+//   interface TextProps {
+//     className?: string;
+//   }
+//   interface ScrollViewProps {
+//     className?: string;
+//   }
+//   interface ImageProps {
+//     className?: string;
+//   }
+//   interface TextInputProps {
+//     className?: string;
+//   }
+//   interface PressableProps {
+//     className?: string;
+//   }
+//   interface TouchableOpacityProps {
+//     className?: string;
+//   }
+//   interface SafeAreaViewProps {
+//     className?: string;
+//   }
+//   interface FlatListProps<ItemT> {
+//     className?: string;
+//     contentContainerStyle?: object;
+//   }
+// }
