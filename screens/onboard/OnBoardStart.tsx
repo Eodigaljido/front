c5db7c38f5e4a9d090b0ad4fd7b83145cef29d64
rlaxogh76 <btm.email2769@gmail.com>
2026-03-23 15:11:50 +0900

    feat: 온보드 시작 화면 컴포넌트 추가

---
+++ screens/onboard/OnBoardStart.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+
+import rooti_map_onboard from "../../assets/onboard/rooti_map_onboard.png";
+
+type RootParamList = {
+  OnBoardStart: undefined;
+  AreaOnBoard: undefined;
+  Tabs: undefined;
+};
+
+export default function OnBoardStart(): React.JSX.Element {
+  const navigation = useNavigation<NativeStackNavigationProp<RootParamList>>();
+
+  return (
+    <SafeAreaView className="flex-1 bg-white" edges={["top"]}>
+      <View className="flex-1 justify-center items-center px-6">
+        <View className="items-center">
+          <View className="flex-row justify-center items-center mb-6">
+            <Image source={rooti_map_onboard} className="w-64 h-64 mx-2" />
+          </View>
+
+          <Text className="text-3xl font-bold text-gray-800 mb-4">
+            나는 어떤 사람일까?
+          </Text>
+
+          <Text className="text-lg text-gray-400 text-center font-bold mb-10">
+            설문 조사를 통해 사용자님에 대한 정보를{"\n"}
+            저희에게 알려주세요!
+          </Text>
+
+          <TouchableOpacity
+            className="bg-blue-500 rounded-full py-3 px-6 w-[275px]"
+            onPress={() => navigation.navigate("AreaOnBoard")}
+          >
+            <Text className="text-white font-bold text-center">시작하기</Text>
+          </TouchableOpacity>
+
+          <Text
+            className="text-base text-gray-400 text-center mt-10 font-bold"
+            onPress={() => navigation.replace("Tabs")}
+          >
+            지금은 안할래요
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
