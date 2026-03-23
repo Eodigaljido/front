bdfc93eb5ae4d402b62a095a72b9a17fd66469e8
rlaxogh76 <btm.email2769@gmail.com>
2026-03-23 15:11:26 +0900

    feat: 온보드용 라디오 버튼 컴포넌트 추가

---
+++ components/onboard/RadioButton.tsx
@@ -0,0 +1,29 @@
+import { TouchableOpacity, View, Text } from "react-native";
+
+export default function RadioButton({
+  label,
+  value,
+  onPress,
+}: {
+  label: string;
+  value: boolean;
+  onPress: () => void;
+}): React.JSX.Element {
+  return (
+    <TouchableOpacity
+      className={`flex-row items-center mt-10 mb-2 rounded-full py-3 px-6 border-2 ${
+        value ? "border-blue-500 bg-blue-500" : "border-gray-300 bg-gray-200"
+      }`}
+      onPress={onPress}
+    >
+      <View className={`w-5 h-10 rounded-full`} />
+      <Text
+        className={`text-base font-bold ${
+          value ? "text-white" : "text-gray-800"
+        }`}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+}
