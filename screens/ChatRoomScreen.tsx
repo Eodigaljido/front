55f89b21f89b7050ec69eead5d47b1da51d033e9
rlaxogh76 <btm.email2769@gmail.com>
2026-04-10 08:42:14 +0900

    feat: ChatRoomScreen 컴포넌트 추가 및 기본 레이아웃 구현

---
+++ screens/ChatRoomScreen.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native";
+
+export const ChatRoomScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg font-bold">Chat room</Text>
+    </View>
+  );
+};
