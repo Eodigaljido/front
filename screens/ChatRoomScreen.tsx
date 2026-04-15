9b516020d8d0c10c560c1614c44215931afebcac
rlaxogh76 <btm.email2769@gmail.com>
2026-04-15 17:10:14 +0900

    feat: ChatRoomScreen에 BubbleChatMock 컴포넌트 추가

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,14 +1,45 @@
 import { RoomHeader } from "@/components/chat/RoomHeader";
 import { RoomFooter } from "@/components/chat/RoomFooter";
-import { ProfileImage } from "@/components/ProfileImage";
 import { View } from "react-native";
-import { Button } from "@/stories/Button";
+
+import { BubbleChat } from "@/stories/chat/BubbleChat";
+
+const BubbleChatMock = () => {
+  return (
+    <View style={{ justifyContent: "flex-end" }}>
+      <BubbleChat
+        isMine={true}
+        sended={true}
+        message="안녕하세요!!!!"
+        timestamp="오후 3:45"
+      />
+      <BubbleChat
+        isMine={false}
+        message="안녕하세요! 반갑습니다."
+        timestamp="오후 3:46"
+        sended={true}
+      />
+      <BubbleChat
+        isMine={true}
+        sended={true}
+        message="반갑습니다잉"
+        timestamp="오후 3:47"
+      />
+      <BubbleChat
+        isMine={true}
+        sended={true}
+        message="동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세"
+        timestamp="오후 3:47"
+      />
+    </View>
+  );
+};
 
 export const ChatRoomScreen = () => {
   return (
     <View className="flex-1">
       <RoomHeader />
-      <View className="flex-1 justify-center items-center"></View>
+      <BubbleChatMock />
       <RoomFooter />
     </View>
   );
