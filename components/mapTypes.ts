adb3549ac858df0318513422d039cd65b3fdfe60
rjsgud49 <qkrrjsgud49@gmail.com>
2026-04-20 10:13:08 +0900

    지도 타입정리

--- components/mapTypes.ts
+++ components/mapTypes.ts
@@ -1,1 +1,16 @@
 export type MapPathPoint = { latitude: number; longitude: number };
+
+export type MapMarkerPoint = {
+  latitude: number;
+  longitude: number;
+  /** 핀 안에 표기할 숫자/텍스트 (예: 1,2,3) */
+  label?: string;
+};
+
+export type MapRouteSegment = {
+  id: string;
+  points: MapPathPoint[];
+  color: string;
+  width?: number;
+  dashed?: boolean;
+};
