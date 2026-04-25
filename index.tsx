35c7dafdbdbfe18fc52444f1d723c82878145ddd
rlaxogh76 <btm.email2769@gmail.com>
2026-04-25 19:57:39 +0900

    fix: 브라우저 전역 변수 폴리필 추가하여 sockjs-client 호환성 개선

--- index.tsx
+++ index.tsx
@@ -3,4 +3,11 @@ import './global.css';
 import { registerRootComponent } from 'expo';
 import App from './App';
 
+// sockjs-client가 참조하는 브라우저 전역 변수 폴리필 (React Native에 없음)
+if (typeof global.navigator === 'undefined') {
+  (global as any).navigator = { onLine: true };
+} else if (typeof (global.navigator as any).onLine === 'undefined') {
+  (global.navigator as any).onLine = true;
+}
+
 registerRootComponent(App);
