43c4baeaebdce4b4b8ef0418123a584f5bcfff50
rlaxogh76 <btm.email2769@gmail.com>
2026-04-26 15:53:04 +0900

    feat: RoomHeader 컴포넌트의 높이를 수정하고 InfoButton 위치 조정

--- components/chat/RoomHeader.tsx
+++ components/chat/RoomHeader.tsx
@@ -1,5 +1,6 @@
 import { BackButton } from "@/components/chat/BackButton";
 import { View, Text } from "react-native";
+import { InfoButton } from "./InfoButton";
 
 interface RoomHeaderProps {
   roomName?: string;
@@ -9,13 +10,18 @@ export const RoomHeader = ({ roomName = "채팅방" }: RoomHeaderProps) => {
   return (
     <View
       className="w-full bg-white flex-row items-center mt-10 px-4"
-      style={{ height: 50 }}
+      style={{ height: 80 }}
     >
       <BackButton />
-      <Text className="text-black text-lg font-bold flex-1 text-center">
-        {roomName}
-      </Text>
-      <View className="w-6" />
+      <View
+        className="absolute left-0 right-0 items-center justify-center"
+        style={{ height: 50 }}
+        pointerEvents="none"
+      >
+        <Text className="text-black text-lg font-bold">{roomName}</Text>
+      </View>
+      <View className="flex-1" />
+      <InfoButton />
     </View>
   );
 };
