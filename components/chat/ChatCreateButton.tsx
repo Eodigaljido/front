362c7a11db97642486786f559adfabc3ca2bc792
rlaxogh76 <btm.email2769@gmail.com>
2026-05-07 14:16:08 +0900

    feat: ChatCreatingButton 컴포넌트에 네비게이션 기능 추가

--- components/chat/ChatCreateButton.tsx
+++ components/chat/ChatCreateButton.tsx
@@ -1,9 +1,16 @@
+import { RootStackParamList } from "@/App";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { Plus } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 
 export const ChatCreatingButton = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   return (
-    <TouchableOpacity className="p-2 rounded-full" activeOpacity={0.7}>
+    <TouchableOpacity
+      className="p-2 rounded-full"
+      activeOpacity={0.7}
+      onPress={() => navigation.navigate("ChatCreatingScreen")}
+    >
       <Plus color="#6C6C6C" strokeWidth={2} />
     </TouchableOpacity>
   );
