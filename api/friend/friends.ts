c733fae044b12409c743f8afc1eb8c9aa3f52cf9
Qlellow <changyeon0702@gmail.com>
2026-05-26 14:01:02 +0900

    Merge branch 'main' of https://github.com/Eodigaljido/front into feature/allscreen

--- api/friend/friends.ts
+++ api/friend/friends.ts
@@ -1,4 +1,4 @@
-import { instance } from "../axios";
+import { instance } from '../axios';
 
 export interface Friends {
   friendId: number;
@@ -10,7 +10,7 @@ export interface Friends {
 
 // 친구 목록 조회
 export async function getFriends(accessToken: string): Promise<Friends[]> {
-  const res = await instance.get<Friends[]>("/friends", {
+  const res = await instance.get<Friends[]>('/api/friends', {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
   return Array.isArray(res.data) ? res.data : [];
@@ -18,15 +18,18 @@ export async function getFriends(accessToken: string): Promise<Friends[]> {
 
 // 내 친구 코드 조회
 export async function getMyFriendCode(): Promise<string> {
-  const res = await instance.get<{ friendCode: string }>("/friends/code");
+  const res = await instance.get<{ friendCode: string }>('/api/friends/code');
   return res.data.friendCode;
 }
 
+// 친구 코드로 친구 추가
+export async function addFriendByCode(friendCode: string): Promise<void> {
+  await instance.post('/api/friends/add', { friendCode });
+}
+
 // 최근 대화한 친구 목록 조회
-export async function getFriendsRecent(
-  accessToken: string,
-): Promise<Friends[]> {
-  const res = await instance.get<Friends[]>("/friends/recent", {
+export async function getFriendsRecent(accessToken: string): Promise<Friends[]> {
+  const res = await instance.get<Friends[]>('/api/friends/recent', {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
   return Array.isArray(res.data) ? res.data : [];
