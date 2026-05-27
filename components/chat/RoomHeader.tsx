90def1aaf705112b8bbdeb831d066bee40c2898d
rlaxogh76 <btm.email2769@gmail.com>
2026-05-27 17:30:02 +0900

    fix: RoomHeader 컴포넌트 스타일 수정 및 코드 정리

--- components/chat/RoomHeader.tsx
+++ components/chat/RoomHeader.tsx
@@ -1,23 +1,29 @@
 import { BackButton } from "@/components/chat/BackButton";
 import { View, Text } from "react-native";
 import { InfoButton } from "./InfoButton";
+import { RouteButton } from "./routeButton";
+import React from "react";
 
 interface RoomHeaderProps {
   roomName?: string;
   roomUuid: string;
 }
 
-export const RoomHeader = ({ roomName = "채팅방", roomUuid }: RoomHeaderProps) => {
+export const RoomHeader = ({
+  roomName = "채팅방",
+  roomUuid,
+}: RoomHeaderProps) => {
   return (
     <View
-      className="w-full bg-white flex-row items-center mt-10 px-4"
+      className="w-full bg-white flex-row items-center mt-5 px-5"
       style={{ height: 80 }}
     >
       <View className="flex-1 items-start">
         <BackButton />
       </View>
       <Text className="text-black text-lg font-bold">{roomName}</Text>
-      <View className="flex-1 items-end">
+      <View className="flex-1 justify-end flex-row" style={{ gap: 20 }}>
+        <RouteButton />
         <InfoButton roomUuid={roomUuid} roomName={roomName} />
       </View>
     </View>
