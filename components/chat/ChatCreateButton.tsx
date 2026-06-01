6eb214a95224ff2cb15fcb5daceaebb3dadbc0ac
rlaxogh76 <btm.email2769@gmail.com>
2026-06-01 23:23:43 +0900

    feat: 채팅 생성 버튼에서 모임 홈으로 네비게이션 변경

--- components/chat/ChatCreateButton.tsx
+++ components/chat/ChatCreateButton.tsx
@@ -1,5 +1,6 @@
 import { rootNavigate } from "@/navigation/rootNavigation";
 import { Plus } from "lucide-react-native";
+import React from "react";
 import { TouchableOpacity } from "react-native";
 
 export const ChatCreatingButton = () => {
@@ -7,7 +8,7 @@ export const ChatCreatingButton = () => {
     <TouchableOpacity
       className="p-2 rounded-full"
       activeOpacity={0.7}
-      onPress={() => rootNavigate("ChatCreatingScreen")}
+      onPress={() => rootNavigate("MeetHome")}
     >
       <Plus color="#6C6C6C" strokeWidth={2} />
     </TouchableOpacity>
